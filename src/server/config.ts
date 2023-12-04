02c349e90ec49672990c82be87117adf22952a5a
Franco Fraticelli <ffraticelli@ycspr.net>
2023-12-03 23:56:20 -0300

    next env

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,5 +1,5 @@
 export const baseUrl =
-  process.env.ENV === "PROD"
+  process.env.NEXT_PUBLIC_ENV === "PROD"
     ? "https://rrhh-management-server-devaaf.onrender.com/"
     : "http://localhost:8000";
 
