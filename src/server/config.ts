27d2778fe59b20287dc40a780eeaffcbbcc4a05b
Franco Fraticelli <ffraticelli@ycspr.net>
2023-12-03 23:59:32 -0300

    api base url correct now

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NEXT_PUBLIC_ENV === "PROD"
-    ? "https://rrhh-management-server-devaaf.onrender.com/"
+    ? "https://rrhh-management-server-devaaf.onrender.com"
     : "http://localhost:8000";
 
 export const sessionPath = "/ses";
